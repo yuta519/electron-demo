ca994aed70c1f95f073a5b03d5589ddbf4e8ecab
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2024-04-05 12:26:23 -0700

    Create preload

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const { app, BrowserWindow } = require('electron')
+// include the Node.js 'path' module at the top of your file
+const path = require('node:path')
 
+// modify your existing createWindow() function
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
   })
 
   win.loadFile('index.html')
